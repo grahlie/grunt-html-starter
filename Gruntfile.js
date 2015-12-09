01bde67fd232ae65d8cfd83410e35d6f97f82eae
grahlie <mathias@grahlie.se>
2015-12-09 18:04:14 +0100

    first ocmmit

---
+++ Gruntfile.js
@@ -0,0 +1,178 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		// Clean build directory
+    clean: {
+      build: {
+        src: 'build'
+      }
+    },
+
+
+
+    // CSS
+    // Compile style.scss and compress it
+    sass: {
+      production: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'build/css/style.css': 'development/sass/style.scss'
+        }
+      },
+      dev: {
+        option: {
+          style: 'expanded'
+        },
+        files: {
+          'build/css/style.css': 'development/sass/style.scss'
+        }
+      }
+    },
+    autoprefixer:{
+      dist:{
+        files:{
+          'build/css/style.css':'build/css/style.css'
+        }
+      }
+    },
+		criticalcss: {
+			home: {
+				options: {
+					url: "http://localhost/html5-sass-template/index.php",
+					width: 1200,
+					height: 900,
+					outputfile: "critical/critical.css",
+					filename: "build/style.css",
+					buffer: 800*1024,
+					ignoreConsole: false
+				}
+			}
+			// Add more pages here "pagename: { ..content.. }"
+		},
+
+
+		
+		// HTML
+		// Copy files, only for development / in production it using minifiers instead
+    copy: {
+      dev: {
+        files: [
+          {expand: true, cwd: 'development/', src: '*.*', dest: 'build/'},
+          {expand: true, cwd: 'development/images', src: '*.*', dest: 'build/images'},
+          {expand: true, cwd: 'development/fonts', src: '*.*', dest: 'build/fonts'}
+        ]
+      },
+      html: {
+        files: [
+          {expand: true, cwd: 'development/', src: '*.*', dest: 'build/'}
+        ]
+      }
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          'development/*.html': 'build/*.html'
+        }
+      }
+    },
+
+
+
+    // IMAGES
+		webp: {
+			files: {
+				expand: true,
+				cwd: 'build/images/',
+				src: ['**/*.png', '**/*.jpg'],
+				dest: 'build/webp/'
+			},
+			options: {
+				quality: 90,
+				alphaQuality: 90
+			}
+		},
+		
+		// JS
+		// Check js files for errors
+    jshint: {
+      all: ['development/**/*.js', '!development/libs/**/*.js']
+    },
+
+    // Minify all js files
+    uglify: {
+      production: {
+        files: {
+          'build/js/scripts.min.js': ['development/**/*.js']
+        }
+      },
+      dev: {
+        options: {
+          mangle: false,
+          beautify: true
+        },
+        files: {
+          'build/js/scripts.min.js': ['development/**/*.js']
+        }
+      }
+    },
+
+
+
+		// WATCH
+		// Watch for changes in js and scss files
+    watch: {
+      css: {
+        files: ['development/sass/style.scss'],
+        tasks: ['sass:dev', 'autoprefixer']
+      },
+      js: {
+        files: ['development/**/*.js'],
+        tasks: ['jshint', 'uglify:dev']
+      },
+      html: {
+        files: ['development/**/*.html', 'development/**/*.php'],
+        tasks: ['copy:html']
+      }
+    },
+    // Updates all browsers
+    browserSync: {
+      bsFiles: {
+        src : ['build/*.html', 'build/*.php', 'build/*.css']
+      },
+      options: {
+        server: {
+          watchTask: true,
+          server: {
+            baseDir: "./build"
+          }
+        }
+      }
+    }
+	});
+
+	// GRUNT LOADS
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+
+
+  // GRUNT TRIGGERS
+  grunt.registerTask('dev', ['clean', 'jshint', 'copy:dev', 'uglify:dev', 'sass:dev', 'autoprefixer', 'watch']);
+  grunt.registerTask('production', ['clean', 'jshint', 'htmlmin', 'uglify:production', 'sass:production', 'autoprefixer', 'watch']);
+  grunt.registerTask('css', ['criticalcss']);
+
+}
---
+++ development/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>title here</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="icon" type="image/png" href="favicon.png">
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="apple-touch-icon-144-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="apple-touch-icon-114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="apple-touch-icon-72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" href="apple-touch-icon-57-precomposed.png">
+
+	<link rel="stylesheet" href="style.css">
+	<script src="libs/js/modernizr.js"></script>
+</head>
+<body>
+
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="js/scripts.js"></script>
+	<script>
+		var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']]; // Tracking code here
+		(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+		g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+		s.parentNode.insertBefore(g,s)}(document,'script'));
+	</script>
+</body>
+</html>
